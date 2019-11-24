aff2dc29ff9a915c36213c2d12d1e420d78d5f34
inf136711 <jakub.r.gorny@student.put.poznan.pl>
2019-11-24 22:24:13 +0100

    Convert class for REST

--- src/main/java/pl/put/poznan/transformer/logic/JSONTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/JSONTransformer.java
@@ -8,21 +8,24 @@ import org.slf4j.Logger;
  * This is just an example to show that the logic should be outside the REST service.
  */
 public class JSONTransformer {
-
-    public JSONArray jsonArray;
-    private final String[] transforms;
     private Logger logger;
+    private JSONArray jsonArray;
+    private String jsonText;
 
-    public JSONTransformer(String text, String[] transforms, Logger logger){
-        this.transforms = transforms;
+    public JSONTransformer(Logger logger) {
         this.logger = logger;
+    }
 
+    public void add(String text){
+        this.jsonText = text;
         try {
             jsonArray = new JSONArray("["+text+"]");
-            logger.debug("jej"); //TODO: usunac glupie komentarze (pozniej)
         } catch (JSONException e) {
-            logger.debug("blad wczytywania do JSONArray");
-            //TODO: handle exception
+            e.printStackTrace();
+            logger.debug("jej");
         }
     }
+    public JSONArray getAll(){ return jsonArray; }
+    public String wypisz(){ return jsonText;}
+
 }
