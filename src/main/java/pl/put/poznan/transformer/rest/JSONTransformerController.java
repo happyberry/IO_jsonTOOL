2173569216906505c7abd604d2cb0e159c00b4ff
inf136711 <jakub.r.gorny@student.put.poznan.pl>
2019-11-24 22:23:39 +0100

    Create working REST

--- src/main/java/pl/put/poznan/transformer/rest/JSONTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JSONTransformerController.java
@@ -2,43 +2,42 @@ package pl.put.poznan.transformer.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.transformer.logic.JSONTransformer;
-
-import java.util.Arrays;
-
+import pl.put.poznan.transformer.logic.*;
 
 @RestController
-@RequestMapping("/{text}")
 public class JSONTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(JSONTransformerController.class);
+    private final JSONTransformer arr = new JSONTransformer(logger);
+    private final JSONMinifier min = new JSONMinifier();
+    private final JSONUnminifier unm = new JSONUnminifier();
+    private final JSONCompare cmp = new JSONCompare(logger);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // do the transformation, you should run your logic here, below just a silly example
-        JSONTransformer transformer = new JSONTransformer(text, transforms, logger);
-        return text;
+    @GetMapping("/minify")
+    public String minify(){
+        String x = min.minify(arr.getAll());
+        return x;
     }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // do the transformation, you should run your logic here, below just a silly example
-        JSONTransformer transformer = new JSONTransformer(text, transforms, logger);
-        return text;
+    @GetMapping("/unminify")
+    public String unminify(){
+        String x = unm.unminify(arr.getAll());
+        return x;
     }
-
+    @PostMapping("/dodaj")
+    public void add(@RequestBody String array){
+        arr.add(array);
+    }
+    @GetMapping("/getAll")
+    public String getAll() {
+        return arr.wypisz();
+    }
+    @PutMapping("/compare")
+    public String[] compare(@RequestBody String ob2){
+        boolean res = cmp.Compare(arr.wypisz(), ob2);
+        String wyn = res ? "True" : "False";
+        String [] wynik = {wyn, cmp.getDifference1(), cmp.getDifference2()};
+        return wynik;
+    }
 
 
 }
