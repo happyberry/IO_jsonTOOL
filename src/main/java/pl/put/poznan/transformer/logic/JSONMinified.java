d337cfc07c0deb07aeb19d7f033c2cd373872429
inf136787 <jakub.raczynski@student.put.poznan.pl>
2019-11-29 19:49:23 +0100

    Dodanie dokumentacji do klas JSONMinified i JSONDeminifed

--- src/main/java/pl/put/poznan/transformer/logic/JSONMinified.java
+++ src/main/java/pl/put/poznan/transformer/logic/JSONMinified.java
@@ -4,7 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 /**
- * REST API controller class, includes implementation of all HTTP requests which are supported by API.
+ * Class containing all the logic and methods needed to minify JSONs.
  *
  * @author Jakub Raczynski
  * @version 1.0
@@ -12,10 +12,21 @@ import org.json.JSONException;
 
 public class JSONMinified extends Decorator {
 
+    /**
+     * Constructor, calls constructor from component passed in constructor's argument.
+     * @param componentDecorating Component from which constructor will be called inside this constructor.
+     */
     public JSONMinified(Component componentDecorating) {
         super(componentDecorating);
     }
 
+    /**
+     * Method which does the minification of the JSON.
+     *
+     * @param jsonString String containing the JSON.
+     * @return String with minified form of the JSON.
+     * @throws JSONException Exception thrown if String doesn't contain correct JSON.
+     */
     public String minify(String jsonString) throws JSONException {
         JSONArray array = JSONTransformer.transform(jsonString);
         String minified = String.valueOf(array);
@@ -23,11 +34,22 @@ public class JSONMinified extends Decorator {
         return minified;
     }
 
+    /**
+     * Method derived from Component interface, wrapper for minify method.
+     *
+     * @throws JSONException Exception thrown if String doesn't contain correct JSON.
+     */
     @Override
     public void Operation() throws JSONException {
         newDecoration = minify(component.getJsonString());
     }
 
+    /**
+     * Stub method derived from Component interface, not needed in this class, so it just returns null.
+     *
+     * @param component Component passed to method just to make it compatible with interface which is implemented by
+     * this class.
+     */
     @Override
     public String Compare(Component component) {
         return null;
--- src/main/java/pl/put/poznan/transformer/logic/JSONUnminified.java
+++ src/main/java/pl/put/poznan/transformer/logic/JSONUnminified.java
@@ -3,12 +3,31 @@ package pl.put.poznan.transformer.logic;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+
+/**
+ * Class containing all the logic and methods needed to deminify JSONs.
+ *
+ * @author Julia Tadej
+ * @version 1.0
+ */
+
 public class JSONUnminified extends Decorator {
 
+    /**
+     * Constructor, calls constructor from component passed in constructor's argument.
+     * @param component Component from which constructor will be called inside this constructor.
+     */
     public JSONUnminified(Component component) {
         super(component);
     }
 
+    /**
+     * Method which does the deminification of the JSON.
+     *
+     * @param text String containing the JSON.
+     * @return String with deminified form of the JSON.
+     * @throws JSONException Exception thrown if String doesn't contain correct JSON.
+     */
     public String unminify(String text) throws JSONException {
         JSONArray array = JSONTransformer.transform(text);
         String unminified = null;
@@ -20,11 +39,22 @@ public class JSONUnminified extends Decorator {
     }
 
 
+    /**
+     * Method derived from Component interface, wrapper for minify method.
+     *
+     * @throws JSONException Exception thrown if String doesn't contain correct JSON.
+     */
     @Override
     public void Operation() throws JSONException {
         newDecoration = unminify(component.getJsonString());
     }
 
+    /**
+     * Stub method derived from Component interface, not needed in this class, so it just returns null.
+     *
+     * @param component Component passed to method just to make it compatible with interface which is implemented by
+     * this class.
+     */
     @Override
     public String Compare(Component component) {
         return null;
