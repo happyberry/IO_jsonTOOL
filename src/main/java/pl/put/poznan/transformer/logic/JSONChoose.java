0142c1844b467e603a1a0c6e0449f1a537757b10
inf136711 <jakub.r.gorny@student.put.poznan.pl>
2020-01-06 12:05:39 +0100

    Create choosing properties method

---
+++ src/main/java/pl/put/poznan/transformer/logic/JSONChoose.java
@@ -0,0 +1,71 @@
+package pl.put.poznan.transformer.logic;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class JSONChoose extends Decorator {
+
+    public JSONChoose(Component component) {super(component);}
+
+    private void chooseNode(JsonNode currentNode, String[] properties) {
+        System.out.println(currentNode.getNodeType());
+        if (currentNode instanceof ObjectNode) {
+            Iterator<String> names = currentNode.fieldNames();
+            List<String> delete = new ArrayList<String>();
+            while(names.hasNext()) {
+                String name = names.next();
+
+                if(!Arrays.asList(properties).contains(name)) {
+                    delete.add(name);
+                    System.out.println(name + " delete");
+                }
+            }
+            if(!delete.isEmpty()) {
+                ((ObjectNode) currentNode).remove(delete);
+            }
+            System.out.println("object");
+        }
+        if (currentNode.isContainerNode()) {
+            System.out.println("container");
+            for (JsonNode child : currentNode) {
+                chooseNode(child, properties);
+            }
+        }
+    }
+
+    public String choose(String jsonString, String[] properties) throws JSONException {
+        if (properties == null) properties = new String[]{};
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode jsonNode = mapper.readTree(jsonString);
+            chooseNode(jsonNode, properties);
+            jsonString = mapper.writeValueAsString(jsonNode);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        JSONArray array = JSONTransformer.transform(jsonString);
+        return array.toString(4);
+    }
+
+    @Override
+    public String operation(String jsonString) throws JSONException {
+        return null;
+    }
+
+    @Override
+    public String operation(String jsonString, String names) throws JSONException {
+        System.out.println(Arrays.toString(names.split(",")));
+        return choose(jsonString, names.split(","));
+    }
+}
